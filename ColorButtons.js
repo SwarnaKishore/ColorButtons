059175436b931ecca85b969ec0b5738200bb633d
Swarna Kishore <swarnakishoree@gmail.com>
2014-07-11 12:29:13 -0500

    Added Buttons

---
+++ ColorButtons.js
@@ -0,0 +1,71 @@
+
+ /* button.onclick = function (e) { selected(this); };  
+  
+function  seleted(cell)
+  {
+  var color = document.getElementById(cell.id);
+    document.body.bgColor=color;
+  }*/
+  
+  
+ function displayChocolate()
+{
+  document.body.bgColor="chocolate";
+}
+ function displayHotPink()
+{
+  document.body.bgColor="HotPink";
+}
+function displayDarkTurquoise() 
+{
+  document.body.bgColor="DarkTurquoise";
+}
+function displayMistyRose() 
+{
+  document.body.bgColor="MistyRose";
+}
+function displayOrchid() 
+{
+  document.body.bgColor="Orchid";
+}
+function displayAquamarine() 
+{
+  document.body.bgColor="Aquamarine";
+}
+function displayBisque() 
+{
+  document.body.bgColor="Bisque";
+}
+function displayLightSeaGreen() 
+{
+  document.body.bgColor="LightSeaGreen";
+}
+function displayLightSalmon() 
+{
+  document.body.bgColor="LightSalmon";
+}
+function displayCoral() 
+{
+  document.body.bgColor="Coral";
+}
+
+function displayGold() 
+{
+  document.body.bgColor="Gold";
+}
+function displayPlum() 
+{
+  document.body.bgColor="Plum";
+}
+function displayCoral() 
+{
+  document.body.bgColor="Coral";
+}
+function displayCoral() 
+{
+  document.body.bgColor="Coral";
+}
+function displayCoral() 
+{
+  document.body.bgColor="Coral";
+}
